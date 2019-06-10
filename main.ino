bd929a53c58a57c4e917fd5842d4ec4ba85a0de8
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2019-06-11 00:21:52 +0200

    Continuously sending a command in Arduino sketch

--- main.ino
+++ main.ino
@@ -1,7 +1,18 @@
 #include "transmit.h"
 
 #define TX_PIN 8
-NEXA_Transmitter nt(8);
+NEXA_Transmitter nt(TX_PIN);
+uint8_t ON_3[] = {0x89, 0x47, 0x4E, 0x6D};
 
-void setup() {}
-void loop() {}
+void setup() {
+	Serial.begin(115200);
+	Serial.print("Starting board. Connect transmitter to pin ");
+	Serial.println(TX_PIN, DEC);
+	nt.send_packet(ON_3, 4);
+}
+
+void loop() {
+	Serial.println("Sending again");
+	delay(1000);
+	nt.send_packet(ON_3, 4);
+}
