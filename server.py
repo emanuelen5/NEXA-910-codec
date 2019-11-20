0e70f9ae5c6bf07c1f97c3b34f06bd29ab891137
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2019-11-20 23:24:05 +0100

    Sending HTTP-400 if arguments not set

--- server.py
+++ server.py
@@ -1,20 +1,27 @@
 from transmit import NEXA_UART
-from flask import Flask, request
+from flask import Flask, request, abort
 app = Flask(__name__)
 
 nexa_uart = NEXA_UART(port="/dev/ttyACM0")
 
 @app.route('/')
-def hello_world():
+def index_route():
     with open("./index.html") as f:
         index_content = f.read()
     return index_content
 
+def get_arg(arg_name):
+    arg = request.args.get(arg_name)
+    if arg is None:
+        abort(400)
+    return arg
+
 @app.route('/lamp', methods=['GET'])
-def lamp():
+def lamp_route():
     print(request.args.to_dict())
-    lamp = request.args.get("lamp")
-    state = request.args.get("state")
-    response = nexa_uart.send_command(" ".join([lamp, state]))
+    lamp = get_arg("lamp")
+    state = get_arg("state")
+    command = " ".join([lamp, state]).strip()
+    response = nexa_uart.send_command(command)
     print(response)
     return dict(OK=True, response=response)
