ef2edfb46d037461c84f566ff2943ec04df74c89
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-07-12 15:39:27 +0200

    Serving static content; refactoring to use enum endpoints + error handling

--- server.py
+++ server.py
@@ -1,19 +1,51 @@
+import traceback
+from logging import getLogger
+from enum import Enum
+
+logger = getLogger(__name__)
+
 from transmit import NEXA_UART
-from flask import Flask, request, abort
-app = Flask(__name__)
+from flask import Flask, request, abort, jsonify, redirect
+from werkzeug.exceptions import HTTPException
+from pathlib import Path
+
+static_path = str(Path(__file__).parent / "static")
+app = Flask(__name__, static_url_path='', static_folder=static_path)
+
+
+@app.errorhandler(Exception)
+def handle_error(e):
+    traceback.print_exception(type(e), e, e.__traceback__)
+    logger.error(e)
+    logger.info(f"Got request: {request}\n\tdata: {request.data}\n\tform: {request.form}\n\targs: {request.args}")
+    code = 500
+    if isinstance(e, HTTPException):
+        code = e.code
+    return jsonify(error=str(e), status=code), code
+
+
+class ENDPOINTS(str, Enum):
+    ROOT      = "/"
+    ROOT_HTML = "/index.html"
+    LAMP      = "/lamp"
+
+    def __str__(self):
+        return self.value
+
 
 class Globals:
-	pass
+    pass
+
+
+@app.route(ENDPOINTS.ROOT)
+def root():
+    return redirect(ENDPOINTS.ROOT_HTML)
+
 
 @app.before_first_request
 def init():
-	Globals.nexa_uart = NEXA_UART.get_connected()[0]
+    Globals.nexa_uart = NEXA_UART.get_connected()[0]
 
-@app.route('/')
-def index_route():
-    with open("./index.html") as f:
-        index_content = f.read()
-    return index_content
 
 def get_arg(arg_name):
     arg = request.args.get(arg_name)
@@ -21,12 +53,11 @@ def get_arg(arg_name):
         abort(400)
     return arg
 
-@app.route('/lamp', methods=['GET'])
+
+@app.route(ENDPOINTS.LAMP, methods=['GET'])
 def lamp_route():
-    print(request.args.to_dict())
     lamp = get_arg("lamp")
     state = get_arg("state")
     command = " ".join([lamp, state]).strip()
     response = Globals.nexa_uart.send_command(command)
-    print(response)
     return dict(OK=True, response=response)
