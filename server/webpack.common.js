7cad5ea40071e8e198b2b167696076ae98e932ed
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-07-23 22:20:05 +0200

    Tweaking build config + copy name fix
    - Fix: Prebundled bootstrap + jquery got wrong path
    - Cleaning output directory before each build
    - Adding progress during build
    - Removing source maps for node modules (e.g. React)

--- server/webpack.common.js
+++ server/webpack.common.js
@@ -1,61 +1,67 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './client/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.html$/,
-        exclude: /node_modules/,
-        use: ['raw-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
-  output: {
-    path: __dirname + '/.build',
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: './.build'
-  },
-  externals: {
-    jquery: 'jQuery',
-    bootstrap: 'bootstrap',
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-    	{ from: 'node_modules/bootstrap/dist/css/*.min.*', to: 'css/'},
-    	{ from: 'node_modules/bootstrap/dist/js/*.min.*', to: 'js/'},
-    	{ from: 'node_modules/jquery/dist/*.min.*', to: 'js/'},
-      ],
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Website main entry point',
-      // Load a custom template (lodash by default)
-      template: './client/index.html'
-    }),
-    new HtmlWebpackTagsPlugin({
-      links: ['css/bootstrap.min.css'],
-    }),
-    new HtmlWebpackTagsPlugin({
-      append: false,
-      scripts: ['js/jquery.min.js', 'js/bootstrap.bundle.min.js'],
-    }),
-  ],
+	entry: {
+			main: './client/index.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			},
+			{
+				test: /\.html$/,
+				exclude: /node_modules/,
+				use: ['raw-loader']
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+		]
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx']
+	},
+	output: {
+		path: __dirname + '/.build',
+		publicPath: '/',
+		filename: '[name].js'
+	},
+	devServer: {
+		contentBase: './.build'
+	},
+	externals: {
+		jquery: 'jQuery',
+		bootstrap: 'bootstrap',
+	},
+	plugins: [
+		new CopyWebpackPlugin({
+			patterns: [
+				{ from: 'node_modules/bootstrap/dist/css/bootstrap.min.*', to: 'css/[name].[ext]'},
+				{ from: 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.*', to: 'js/[name].[ext]'},
+				{ from: 'node_modules/jquery/dist/jquery.min.*', to: 'js/[name].[ext]'},
+			],
+		}),
+		new HtmlWebpackPlugin({
+			title: 'Website main entry point',
+			// Load a custom template (lodash by default)
+			template: './client/index.html'
+		}),
+		new HtmlWebpackTagsPlugin({
+			links: ['css/bootstrap.min.css'],
+		}),
+		new HtmlWebpackTagsPlugin({
+			append: false,
+			scripts: ['js/jquery.min.js', 'js/bootstrap.bundle.min.js'],
+		}),
+		new webpack.ProgressPlugin(),
+		new CleanWebpackPlugin(),
+	],
 };
--- server/webpack.dev.js
+++ server/webpack.dev.js
@@ -1,10 +1,19 @@
+const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: "development",
-    devtool: "eval-source-map",
+    devtool: false,
     devServer: {
         contentBase: './.build',
     },
+    plugins: [
+        new webpack.EvalSourceMapDevToolPlugin({
+            filename: '[name].js.map',
+            exclude: [
+                /node_modules/
+            ]
+        })
+    ],
 });
