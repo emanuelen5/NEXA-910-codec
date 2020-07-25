2bc0c5f749f00d7d065babf17ab56b4fcacab046
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-07-25 09:32:57 +0200

    Do not remove HTML on rebuild of JS

--- server/webpack.common.js
+++ server/webpack.common.js
@@ -5,63 +5,65 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-	entry: {
-			main: './client/index.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: ['babel-loader']
-			},
-			{
-				test: /\.html$/,
-				exclude: /node_modules/,
-				use: ['raw-loader']
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			},
-		]
-	},
-	resolve: {
-		extensions: ['*', '.js', '.jsx']
-	},
-	output: {
-		path: __dirname + '/.build',
-		publicPath: '/',
-		filename: '[name].js'
-	},
-	devServer: {
-		contentBase: './.build'
-	},
-	externals: {
-		jquery: 'jQuery',
-		bootstrap: 'bootstrap',
-	},
-	plugins: [
-		new CopyWebpackPlugin({
-			patterns: [
-				{ from: 'node_modules/bootstrap/dist/css/bootstrap.min.*', to: 'css/[name].[ext]'},
-				{ from: 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.*', to: 'js/[name].[ext]'},
-				{ from: 'node_modules/jquery/dist/jquery.min.*', to: 'js/[name].[ext]'},
-			],
-		}),
-		new HtmlWebpackPlugin({
-			title: 'Website main entry point',
-			// Load a custom template (lodash by default)
-			template: './client/index.html'
-		}),
-		new HtmlWebpackTagsPlugin({
-			links: ['css/bootstrap.min.css'],
-		}),
-		new HtmlWebpackTagsPlugin({
-			append: false,
-			scripts: ['js/jquery.min.js', 'js/bootstrap.bundle.min.js'],
-		}),
-		new webpack.ProgressPlugin(),
-		new CleanWebpackPlugin(),
-	],
+    entry: {
+            main: './client/index.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                use: ['raw-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/.build',
+        publicPath: '/',
+        filename: '[name].js'
+    },
+    devServer: {
+        contentBase: './.build'
+    },
+    externals: {
+        jquery: 'jQuery',
+        bootstrap: 'bootstrap',
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'node_modules/bootstrap/dist/css/bootstrap.min.*', to: 'css/[name].[ext]'},
+                { from: 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.*', to: 'js/[name].[ext]'},
+                { from: 'node_modules/jquery/dist/jquery.min.*', to: 'js/[name].[ext]'},
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Website main entry point',
+            // Load a custom template (lodash by default)
+            template: './client/index.html'
+        }),
+        new HtmlWebpackTagsPlugin({
+            links: ['css/bootstrap.min.css'],
+        }),
+        new HtmlWebpackTagsPlugin({
+            append: false,
+            scripts: ['js/jquery.min.js', 'js/bootstrap.bundle.min.js'],
+        }),
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: ['!index.html'],
+        }),
+    ],
 };
