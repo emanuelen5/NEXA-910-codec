bf4be7f9c5ba8d33043a15a9f23ab87c52fe40e6
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-11-02 22:03:04 +0100

    Explicit copy paths

--- server/webpack.common.js
+++ server/webpack.common.js
@@ -44,10 +44,10 @@ module.exports = {
     plugins: [
         new CopyWebpackPlugin({
             patterns: [
-                { from: 'node_modules/bootstrap/dist/css/bootstrap.min.*', to: 'css/[name].[ext]'},
-                { from: 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.*', to: 'js/[name].[ext]'},
+                { from: 'node_modules/bootstrap/dist/css/bootstrap.min.css{,.map}', to: 'css/[name].[ext]'},
+                { from: 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js{,.map}', to: 'js/[name].[ext]'},
                 { from: 'node_modules/jquery/dist/jquery.min.*', to: 'js/[name].[ext]'},
-                { from: 'node_modules/font-awesome/css/font-awesome.min.*', to: 'css/[name].[ext]'},
+                { from: 'node_modules/font-awesome/css/font-awesome.{css.map,min.css}', to: 'css/[name].[ext]'},
                 { from: 'node_modules/font-awesome/fonts/*.woff2', to: 'fonts/[name].[ext]'},
             ],
         }),
